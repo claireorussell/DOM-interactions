52456d8fb713784ef98a8d81db3f4aa7fa2a97e6
claire-russell <russell.claireolivia@gmail.com>
2019-11-30 15:12:40 +1300

    Dot game counter added

--- game.js
+++ game.js
@@ -12,18 +12,28 @@ function bindEventListeners (dots) {
     // BIND YOUR EVENT LISTENERS HERE
     // The first one is provided for you
     dots[i].addEventListener('contextmenu', makeGreen)
+    dots[i].addEventListener('click', makeBlue)
+    dots[i].addEventListener('dblclick', hide)
   }
 }
 
 function makeGreen (evt) {
   evt.preventDefault()
-  evt.target.classList.toggle('green')
+  evt.target.classList.toggle('green') // event 'evt' objects you've 'targeted'. classList is an object with a method attached 
   updateCounts()
 }
 
 // CREATE FUNCTION makeBlue HERE
+function makeBlue (evt){
+  evt.target.classList.toggle('blue')
+  updateCounts()
+}
 
 // CREATE FUNCTION hide HERE
+function hide (evt) {
+  evt.target.classList.toggle('invisible')
+  updateCounts()
+}
 
 function updateCounts () {
   var totals = {
@@ -34,6 +44,14 @@ function updateCounts () {
   
   // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
 
+  var blueCount =  document.getElementsByClassName("blue").length
+  totals.blue = blueCount
+  var greenCount =  document.getElementsByClassName("green").length
+  totals.green = greenCount
+  var invisibleCount =  document.getElementsByClassName("invisible").length
+  totals.invisible = invisibleCount
+
+
   // Once you've done the counting, this function will update the display
   displayTotals(totals)
 }
@@ -41,5 +59,6 @@ function updateCounts () {
 function displayTotals (totals) {
   for (var key in totals) {
     document.getElementById(key + '-total').innerHTML = totals[key]
+    
   }
 }
